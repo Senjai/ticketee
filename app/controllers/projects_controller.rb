8b7bb11f865ec801c505bd4f655f77d27c424226
Senjai <r.crawfordwilson@gmail.com>
2013-08-16 21:06:46 -0700

    Made scenario 'can not create a project without a name' pass in spec/features/creating_projects_spec.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,7 +18,8 @@ class ProjectsController < ApplicationController
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # do nothing yet
+      flash[:alert] = 'Project has not been created.'
+      render action: :new
     end
   end
 
