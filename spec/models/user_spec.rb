019f36a519aa7aad545525f45820fcb19bbdd540
Senjai <r.crawfordwilson@gmail.com>
2013-08-18 01:39:04 -0700

    Tested has_secure_password and made sure it was passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "passwords" do
+    it "needs a password and confirmation to save" do
+      u = User.new(name: "steve")
+
+      u.save
+      expect(u).to_not be_valid
+
+      u.password = "password"
+      u.password_confirmation = ""
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = "password"
+      u.save
+      expect(u).to be_valid
+    end
+
+    it "needs a password and confirmation to match" do
+      u = User.create(name: "steve", password: 'hunter2', password_confirmation: 'hunter')
+      expect(u).to_not be_valid
+    end
+  end
+end
+
+describe "authentication" do
+  let(:user) { User.create(name: "steve", password: "hunter2", password_confirmation: "hunter2") }
+
+  it "authenticates with a correct password" do
+    expect(user.authenticate("hunter2")).to be
+  end
+
+  it "does not authenticate with a wrong password" do
+    expect(user.authenticate("hunter1")).to_not be
+  end
 end
