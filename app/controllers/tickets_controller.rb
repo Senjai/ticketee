bfa21ca317887f306a932d7efa27added85fa2b5
Senjai <r.crawfordwilson@gmail.com>
2013-08-18 00:27:28 -0700

    Made the create ticket feature pass. Generated model and controller along with actions and views.

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,35 @@
+class TicketsController < ApplicationController
+  before_action :set_project
+  before_action :set_ticket, only: SEUD
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def show
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    if @ticket.save
+      redirect_to [@project, @ticket], notice: "Ticket has been created."
+    else
+      flash[:alert] = "Ticket has not been created."
+      render action: "new"
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
+  end
+end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -24,4 +24,14 @@ feature "Creating Tickets" do
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank")
   end
+
+  scenario "Description must be longer than 10 characters" do
+    fill_in "Title", with: "Non-standards compliance"
+    fill_in "Description", with: "it sucks"
+    click_button "Create Ticket"
+
+    expect(page).to have_content("Ticket has not been created.")
+    expect(page).to have_content("Description is too short")
+  end
+
 end
