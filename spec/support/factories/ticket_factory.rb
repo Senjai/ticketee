b91701c3914b8ce434f2b0d29658305f0857afde
Senjai <r.crawfordwilson@gmail.com>
2013-08-18 22:31:55 -0700

    Added authentication helper, made specs pass after breaking them with authentication requirements

--- spec/support/factories/ticket_factory.rb
+++ spec/support/factories/ticket_factory.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :ticket do
     title "Example Ticket"
     description "An example ticket, nothing more"
+    association :user, factory: :user
+    association :project, factory: :project
   end
 end
