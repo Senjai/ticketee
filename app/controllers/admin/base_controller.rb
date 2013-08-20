6c7d3c03abde3b9016dce9089bdd1caf341a669d
Senjai <r.crawfordwilson@gmail.com>
2013-08-19 22:59:26 -0700

    Added admin user creation.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,10 @@
 class Admin::BaseController < ApplicationController
   before_filter :authorize_admin!
 
+  def index
+
+  end
+
   private
 
   def authorize_admin!
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,27 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.order(:email)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.password_confirmation ||= @user.password
+
+    if @user.save
+      redirect_to admin_users_path, notice: "User has been created."
+    else
+      flash.now[:alert] = "User has not been created."
+      render action: :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
