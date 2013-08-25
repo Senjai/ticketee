19c0920974470cbbf210437f2c706c046b170f9a
Senjai <r.crawfordwilson@gmail.com>
2013-08-25 10:39:26 -0700

    Fixed binding in ability class. Refactored DB Clean script

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,9 @@ class Ability
   def initialize(user)
     user.permissions.each do |permission|
       can permission.action.to_sym,
-          permission.thing_type.constantize {|thing| thing.nil? || permission.thing.nil? || permission.thing_id == thing.id}
+          permission.thing_type.constantize do |thing|
+            thing.nil? || permission.thing.nil? || permission.thing_id == thing.id
+          end
     end
   end
 end
--- script/cleantdb.rb
+++ script/cleantdb.rb
@@ -1,17 +1,17 @@
-MODEL_DIR = File.expand_path("app/models")
-
-Dir.chdir(MODEL_DIR)
-files = Dir.glob(File.join("**", "*.rb"))
-
-files.map! do |file|
-  file[0..-4].pluralize
-end
-
 print "This will WIPE your database. Continue? (y/n): "
+
 if $stdin.gets.chomp.downcase == "y"
-  files.each do |f|
-    puts "Wiping #{f}.."
-    ActiveRecord::Base.connection.execute "TRUNCATE TABLE #{f};"
+  # iterate over all model definition files
+  Dir["#{Rails.root}/app/models/**/*.rb"].map do |model_filename|
+    # get the file base_name
+    model_file_basename = File.basename(model_filename,File.extname(model_filename))
+    # get the model class from the file basename
+    model_class = model_file_basename.camelize.constantize
+    # ask the model (ActiveRecord::Base subclass) to give you its table_name
+    table_name = model_class.table_name
+    # wipe the table
+    puts "Wiping table #{table_name}.."
+    ActiveRecord::Base.connection.execute "TRUNCATE TABLE #{table_name};"
   end
 else
   puts "Terminating script..."
